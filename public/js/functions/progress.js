9b7b355f36dc8b7278eef6125ebed58846667bed
Mauro <molinamauro2@gmail.com>
2020-09-23 22:29:50 -0300

    added percentage value to progress bar

--- public/js/functions/progress.js
+++ public/js/functions/progress.js
@@ -8,11 +8,13 @@ export const updateProgress = () => {
         const completeTasks = document.querySelectorAll('i.completo');
         const progress = Math.round(( completeTasks.length / tasks.length) * 100);
         const percentage = document.querySelector('#porcentaje');
+        const percentageP = document.querySelector('p.progress');
+        percentageP.innerHTML = progress + '%';
         percentage.style.width = progress+'%';
         if(progress === 100) {
             Swal.fire(
                 'Felicidades!',
-                'Completaste tu proyecto!',
+                'Este proyecto está completo!',
                 'success'
                 )
         }
